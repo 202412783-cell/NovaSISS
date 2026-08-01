30bb618f44f7f68124145f5a57fef336cb3bf67d
202412783-celda <202412783@est.umss.edu>
2026-08-01 12:26:40 -0400

    configurar database

--- novasiss/settings.py
+++ novasiss/settings.py
@@ -91,13 +91,10 @@ import dj_database_url
 
 DATABASES = {
     "default": dj_database_url.config(
-        default=os.environ.get("DATABASE_URL"),
-        conn_max_age=600,
-        ssl_require=True
+        default="sqlite:///db.sqlite3",
+        conn_max_age=600
     )
 }
-
-
 
 # PASSWORD VALIDATION
 
